30e11e8c1eff39a726b07ebaf2668acfac3b3821
Sebastian <spieta2@gmail.com>
2020-03-16 02:14:08 +0100

    progress

---
+++ talod-game/Assets/Scripts/DeerController.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(Animator))]
+public class DeerController : MonoBehaviour
+{
+    [SerializeField]
+    private bool isBaby = false;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float walkRadius;
+    [SerializeField]
+    private Vector2 waitTimeMinMax;
+    [SerializeField]
+    private Vector2 breedTimeMinMax;
+    [SerializeField]
+    private GameObject babyPrefab;
+
+    private Animator animator;
+
+    private Vector2 startPos;
+    private Vector2 nextPos;
+    private float proximity = 0.2f;
+
+    private float waitTimer;
+    private float growingTimer = 30f;
+    private float makingBBTimer;
+
+    private bool isInFences;
+    private bool isFed;
+    private bool isWatered;
+
+    private TextMeshProUGUI timerText;
+    private GameObject waterNeed;
+    private GameObject foodNeed;
+
+    private Transform grabPoint;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        timerText = transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+        waterNeed = transform.GetChild(0).GetChild(1).GetChild(0).gameObject;
+        foodNeed = transform.GetChild(0).GetChild(1).GetChild(1).gameObject;
+
+        timerText.SetText("");
+        waterNeed.SetActive(false);
+        foodNeed.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        nextPos = startPos;
+        animator.SetBool("isBaby", isBaby);
+        makingBBTimer = Random.Range(breedTimeMinMax.x, breedTimeMinMax.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"Deer: fence {isInFences}, fed: {isFed}, water: {isWatered}, adult: {!isBaby}");
+        if(isInFences)
+        {
+            if (!isFed || !isWatered)
+            {
+                foodNeed.SetActive(!isFed);
+                waterNeed.SetActive(!isWatered);
+                timerText.SetText("");
+                return;
+            }
+            else
+            {
+                foodNeed.SetActive(false);
+                waterNeed.SetActive(false);
+            }
+            if (isBaby)
+            {
+                if (growingTimer > 0)
+                {
+                    growingTimer -= Time.deltaTime;
+                    timerText.SetText(growingTimer.ToString("F0")); ;
+                }
+                else
+                {
+                    isBaby = false;
+                    animator.SetBool("isBaby", false);
+                    isFed = false;
+                    isWatered = false;
+                }
+                
+            }
+            else
+            {
+                if (makingBBTimer > 0)
+                {
+                    makingBBTimer -= Time.deltaTime;
+                    timerText.SetText(makingBBTimer.ToString("F0")); ;
+                }
+                else
+                {
+                    makingBBTimer = Random.Range(breedTimeMinMax.x, breedTimeMinMax.y);
+                    isFed = false;
+                    isWatered = false;
+                    MakeBaby();
+                }
+            }
+            if (transform.localScale.x < 0) timerText.rectTransform.localScale = new Vector3(-1f, 1f, 1f);
+            else timerText.rectTransform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        if(grabPoint != null)
+        {
+            transform.position = grabPoint.position;
+            return;
+        }
+
+        if(waitTimer>0)
+        {
+            animator.SetFloat("speed", 0f);
+            waitTimer -= Time.deltaTime;
+            return;
+        }
+
+        if (Mathf.Abs((new Vector2(transform.position.x, transform.position.y) - nextPos).magnitude) < proximity)
+        {
+            waitTimer = Random.Range(waitTimeMinMax.x, waitTimeMinMax.y);
+            nextPos = new Vector2(Random.Range(startPos.x - walkRadius, startPos.x + walkRadius), Random.Range(startPos.y - walkRadius, startPos.y + walkRadius));
+            if (transform.position.x - nextPos.x < 0) transform.localScale = new Vector3(-1f, 1f, 1f);
+            else transform.localScale = Vector3.one;
+        }
+        animator.SetFloat("speed", speed);
+        transform.position = Vector2.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    public void Grab(Transform grabPoint)
+    {
+        animator.SetFloat("speed", 0f);
+        timerText.SetText("");
+        this.grabPoint = grabPoint;
+        isInFences = false;
+        waterNeed.SetActive(false);
+        foodNeed.SetActive(false);
+    }
+
+    public void Release(bool isInFences)
+    {
+        this.isInFences = isInFences;
+        nextPos = grabPoint.position;
+        startPos = nextPos;
+        grabPoint = null;
+        transform.position = nextPos;
+        if(isInFences)
+        {
+            waterNeed.SetActive(!isWatered);
+            foodNeed.SetActive(!isFed);
+        }
+    }
+
+    private void MakeBaby()
+    {
+        Instantiate(babyPrefab);
+    }
+
+    public void Feed()
+    {
+        if (isInFences) isFed = true;
+    }
+
+    public void Water()
+    {
+        if (isInFences) isWatered = true;
+    }
+}
---
+++ talod-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float crouchSlowdown;
+    [SerializeField]
+    private float sprintIncrease;
+    [SerializeField]
+    private Transform grabPoint;
+    [SerializeField]
+    private float grabRadius;
+    [SerializeField]
+    private float waterRadius;
+    [SerializeField]
+    private float feedRadius;
+
+    private new Rigidbody2D rigidbody;
+    private Animator animator;
+
+    private DeerController grabbedDeer;
+    private bool isGrabbing = false;
+    private bool grabCheck = true;
+    private bool isInFences = false;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Feeding();
+        Watering();
+        Throwing();
+        Grabbing();
+        GrabChecker();
+        Movement();
+    }
+
+    private void Feeding()
+    {
+        if (isGrabbing) return;
+        if(Input.GetButtonDown("Feed"))
+        {
+            animator.SetTrigger("feed");
+            Collider2D[] deers = Physics2D.OverlapCircleAll(grabPoint.position, feedRadius, 1 << LayerMask.NameToLayer("Deers"));
+            foreach(Collider2D d in deers)
+            {
+                DeerController dc = d.GetComponent<DeerController>();
+                if (dc != null) dc.Feed();
+            }
+        }
+    }
+
+    private void Watering()
+    {
+        if (isGrabbing) return;
+        if(Input.GetButtonDown("Water"))
+        {
+            animator.SetTrigger("water");
+            Collider2D[] deers = Physics2D.OverlapCircleAll(grabPoint.position, waterRadius, 1 << LayerMask.NameToLayer("Deers"));
+            foreach (Collider2D d in deers)
+            {
+                DeerController dc = d.GetComponent<DeerController>();
+                if (dc != null) dc.Water();
+            }
+        }
+    }
+
+    private void Grabbing()
+    {
+        if (isGrabbing) return;
+        if (!grabCheck) return;
+        if(Input.GetButtonDown("Grab"))
+        {
+            Collider2D deer = Physics2D.OverlapCircle(grabPoint.position, grabRadius, 1 << LayerMask.NameToLayer("Deers"));
+            if (deer == null) return;
+            grabbedDeer = deer.GetComponent<DeerController>();
+            grabbedDeer.Grab(grabPoint);
+            isGrabbing = true;
+            animator.SetTrigger("grab");
+            grabCheck = false;
+        }
+    }
+
+    private void Throwing()
+    {
+        if (!isGrabbing) return;
+        if (!grabCheck) return;
+        if (Input.GetButtonDown("Grab"))
+        {
+            isGrabbing = false;
+            animator.SetTrigger("yeet");
+            grabbedDeer.Release(isInFences);
+            grabbedDeer = null;
+            grabCheck = false;
+        }
+    }
+
+    private void GrabChecker()
+    {
+        if (Input.GetButtonUp("Grab")) grabCheck = true;
+    }
+
+    private void Movement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        float speedModifier = Input.GetButton("Crouch") ? crouchSlowdown : 1f;
+        speedModifier = Input.GetButton("Sprint") ? sprintIncrease : speedModifier;
+        animator.SetFloat("speedMod", speedModifier);
+
+        rigidbody.velocity = new Vector2(moveX * speed * speedModifier, moveY * speed * speedModifier);
+        animator.SetFloat("walkSpeed", Mathf.Abs(rigidbody.velocity.magnitude));
+
+        if (moveX < 0f) transform.localScale = new Vector3(-1f, 1f, 1f);
+        else transform.localScale = Vector3.one;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Fences"))
+        {
+            isInFences = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Fences"))
+        {
+            isInFences = false;
+        }
+    }
+}
